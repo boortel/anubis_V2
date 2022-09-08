5a4703f2a12f66d0cdd852a702d5a1b36912a6fa
211157 <211157@vut.cz>
2022-09-08 12:02:51 +0200

    uart settings update

--- src/uart_communication.py
+++ src/uart_communication.py
@@ -1,48 +1,54 @@
 import serial
 import serial.tools.list_ports
 
+baudrate_sett = 9600
+parity_sett = serial.PARITY_NONE
+bytesize_sett = serial.EIGHTBITS
+stopbits_sett = serial.STOPBITS_ONE
+
+
 def set_light(intensity, lightID, comport):
     with serial.Serial(comport, 
-                    baudrate=9600, 
-                    parity=serial.PARITY_NONE, 
-                    bytesize=serial.EIGHTBITS, 
-                    stopbits=serial.STOPBITS_ONE) as port:
+                    baudrate=baudrate_sett, 
+                    parity=parity_sett, 
+                    bytesize=bytesize_sett, 
+                    stopbits=stopbits_sett) as port:
         msg = bytes(f'$set_light;{intensity};{lightID}\n', "utf-8")
         port.write(msg)
 
 def set_speed(intensity, comport):
     with serial.Serial(comport, 
-                    baudrate=9600, 
-                    parity=serial.PARITY_NONE, 
-                    bytesize=serial.EIGHTBITS, 
-                    stopbits=serial.STOPBITS_ONE) as port:
+                    baudrate=baudrate_sett, 
+                    parity=parity_sett, 
+                    bytesize=bytesize_sett, 
+                    stopbits=stopbits_sett) as port:
         msg = bytes(f'$set_speed;{intensity}\n', "utf-8")
         port.write(msg)
 
 def change_direction(direction, comport):
     with serial.Serial(comport, 
-                    baudrate=9600, 
-                    parity=serial.PARITY_NONE, 
-                    bytesize=serial.EIGHTBITS, 
-                    stopbits=serial.STOPBITS_ONE) as port:
+                    baudrate=baudrate_sett, 
+                    parity=parity_sett, 
+                    bytesize=bytesize_sett, 
+                    stopbits=stopbits_sett) as port:
         msg = bytes(f'$change_direction;{direction}\n', "utf-8")
         port.write(msg)
 
 def toggle_rotation(state, comport):
     with serial.Serial(comport, 
-                    baudrate=9600, 
-                    parity=serial.PARITY_NONE, 
-                    bytesize=serial.EIGHTBITS, 
-                    stopbits=serial.STOPBITS_ONE) as port:
+                    baudrate=baudrate_sett, 
+                    parity=parity_sett, 
+                    bytesize=bytesize_sett, 
+                    stopbits=stopbits_sett) as port:
         msg = bytes(f'$toggle_rotation;{state}\n', "utf-8")
         port.write(msg)
     
 def toggle_light(state, lightID, comport):
     with serial.Serial(comport, 
-                    baudrate=9600, 
-                    parity=serial.PARITY_NONE, 
-                    bytesize=serial.EIGHTBITS, 
-                    stopbits=serial.STOPBITS_ONE) as port:
+                    baudrate=baudrate_sett, 
+                    parity=parity_sett, 
+                    bytesize=bytesize_sett, 
+                    stopbits=stopbits_sett) as port:
         msg = bytes(f'$toggle_light;{state};{lightID}\n', "utf-8")
         port.write(msg)
     
